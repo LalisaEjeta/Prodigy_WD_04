d4d2cb42d865eec1d10307a7796856780ea8be38
Lalisa-Ejeta <lalisaaejjetaa@gmail.com>
2024-03-08 14:12:09 +0300

    first commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,11 @@
+setTimeout(function () {
+  $(".about-info-inner").typed({
+    strings: [
+      "<span>Hi, I am Lalisa Ejeta</span> I am full stack  web developer and an AI engineer who aims to be the best version of me. I have completed couple of projects to upgrade my web developing skill. I love coding for that I code every day. I am quick and self learner. I am to be graduated my masters degree in computer science from University of Electronic Science and Technology of China. <br/> I am responsible, organized and plan oriented man. Hardworking and self discipline are what I value the most along with being creative and problem  solving. I also love to work with group of people to share experience with others and to solve problem together",
+    ],
+    typeSpeed: 1,
+    contentType: "html",
+  });
+}, 200);
+
+
